53d2b4a6097d2982193361972f3d069edc56537d
Weddy Gikunda <110189834+caviere@users.noreply.github.com>
2022-12-23 18:25:02 +0300

    Update sample.py

--- netcdf/sample.py
+++ netcdf/sample.py
@@ -6,13 +6,13 @@ import zarr
 nc_file = nc.Dataset('data.nc', 'w', format='NETCDF4')
 
 foo_group = nc_file.createGroup("foo")
-bar_group = foo_group.createGroup("bar")
+bar_group = nc_file.createGroup("bar")
 
 dim = nc_file.createDimension('dim', 100)
 
 foo = nc_file.createVariable('aaa', 'i4', ('dim', 'dim'), chunksizes=(10,10))
 
-bar = nc_file.createVariable('bbbr', 'i4', ('dim', 'dim'),chunksizes=(10,10))
+bar = nc_file.createVariable('bbb', 'i4', ('dim', 'dim'),chunksizes=(10,10))
 
 foo[:,:] = 100
 
