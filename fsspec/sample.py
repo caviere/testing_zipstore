b25b4134737b286ef3299610eadd24c7684c0eb2
Weddy Gikunda <110189834+caviere@users.noreply.github.com>
2023-01-09 22:18:40 +0300

    Update sample.py

--- fsspec/sample.py
+++ fsspec/sample.py
@@ -3,11 +3,9 @@ import zarr
 
 fs = fsspec.filesystem('zip:///path/to/myfile.zip')
 
-
 store = zarr.ZipStore('data.zip', mode='w')
 root = zarr.group(store)
 foo = root.create_group("foo")
-    
 foo = foo.zeros('aaa', shape=(10000, 10000), chunks=(100, 100), dtype='i4')
 foo[:] = 10000
 
@@ -15,7 +13,6 @@ foo.flush()
 store.close()
 
 #opening the file is used in a with context
-
 with fs.open('myfile.zip', mode='r') as f:
     print(root.info)
     
