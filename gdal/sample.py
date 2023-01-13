73503289b1ba221dbf28ddcab05ce5372c4814d1
Weddy Gikunda <110189834+caviere@users.noreply.github.com>
2023-01-13 15:17:58 +0300

    refactoring the code

--- gdal/sample.py
+++ gdal/sample.py
@@ -1,23 +1,24 @@
 import numpy as np
-import sys
-from osgeo import ogr
+from osgeo import gdal
 import zarr
 
+def main():
+    store = zarr.ZipStore('data.zip', mode='w')
+    root = zarr.group(store)
+    foo = root.create_group("foo")
 
-driver = ogr.GetDriverByName('ZARR')
+    foo = foo.zeros('aaa', shape=(10000, 10000), chunks=(100, 100), dtype='i4')
+    foo[:] = 10000
+    store.close()
     
-store = zarr.ZipStore('data.zip', mode='w')
+    # open the zarr store using the zarr driver
+    ds = gdal.Open("/vsizip/data.zip")
 
-root = zarr.group(store)
-foo = root.create_group("foo")
+    # create a dataset using the zarr driver
+    foo_ds = gdal.GetDriverByName("ZARR").Create("foo_ds", 10000, 10000, 1)
+    foo_ds.GetRasterBand(1).WriteArray(np.ones((10000, 10000)))
+    foo_ds = None
 
-foo = foo.zeros('aaa', shape=(10000, 10000), chunks=(100, 100), dtype='i4')
+if __name__ == "__main__":
+    main()
 
-foo[:] = 10000
-store.close()
-
-ds = ogr.Open('data.zip')
-foo_ds = driver.Create("/foo/aaa", 10000, 10000, 1, ogr.GDT_Int32)
-foo_ds.WriteArray(np.ones((10000, 10000)))
-
-foo_ds = None
