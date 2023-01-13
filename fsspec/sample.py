9a0e15f526df8d2c7cd16aa91269e4988f8d0771
Weddy Gikunda <110189834+caviere@users.noreply.github.com>
2023-01-13 16:02:40 +0300

    add fsspec implementation

--- fsspec/sample.py
+++ fsspec/sample.py
@@ -1,19 +1,22 @@
 import fsspec
 import zarr
 
-fs = fsspec.filesystem('zip:///path/to/myfile.zip')
+def main():
+    fs = fsspec.filesystem('zip:///path/to/myfile.zip')
+    
+    store = zarr.ZipStore('data.zip', mode='w')
+    root = zarr.group(store)
+    foo = root.create_group("foo")
+    foo = foo.zeros('aaa', shape=(10000, 10000), chunks=(100, 100), dtype='i4')
+    foo[:] = 10000
+    foo.flush()
+    store.close()
 
-store = zarr.ZipStore('data.zip', mode='w')
-root = zarr.group(store)
-foo = root.create_group("foo")
-foo = foo.zeros('aaa', shape=(10000, 10000), chunks=(100, 100), dtype='i4')
-foo[:] = 10000
+    #opening the file is used in a with context
+    with fs.open('myfile.zip', mode='r') as f:
+        print(root.info)
 
-foo.flush()
-store.close()
-
-#opening the file is used in a with context
-with fs.open('myfile.zip', mode='r') as f:
-    print(root.info)
+if __name__ == "__main__":
+    main()
     
 
