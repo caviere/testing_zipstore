255e3bed6ec244aeab6b9ca1091120f5f62bb30e
Weddy Gikunda <110189834+caviere@users.noreply.github.com>
2023-01-13 16:27:46 +0300

    add netcdf implementation

--- netcdf/sample.py
+++ netcdf/sample.py
@@ -3,30 +3,26 @@ import xarray as xr
 import numpy as np
 import zarr
 
-nc_file = xr.Dataset(
-    data_vars={
+def main():
+    nc_file = xr.Dataset(
+        data_vars={
         'foo': (('dim', 'dim'), np.full((10000, 10000), 10000)),
         'bar': (('dim', 'dim'), np.full((10000, 10000), 10000))
-    },
-    coords={
+        },
+        coords={
         'dim': range(10000)
-    }
-)
+        }
+    )
 
-store = zarr.ZipStore('data.zip', mode='w')
+    store = zarr.ZipStore('data.zip', mode='w')
+    encoding ={'foo': {'chunks': (100,100)}, 'bar': {'chunks': (100,100)}}
+    zarr_group = nc_file.to_zarr(store, encoding=encoding)
+    store.close()
+    store = zarr.ZipStore('data.zip', mode='r')
+    zarr_group = zarr.open_group(store)
+    # print(nc_file.foo)
+    nc_file.close()
+    store.close()
 
-encoding ={'foo': {'chunks': (100,100)}, 'bar': {'chunks': (100,100)}}
-
-zarr_group = nc_file.to_zarr(store, encoding=encoding)
-
-store.close()
-
-store = zarr.ZipStore('data.zip', mode='r')
-
-zarr_group = zarr.open_group(store)
-
-print(nc_file.foo)
-
-nc_file.close()
-
-store.close()
+if __name__ == "__main__":
+    main()
